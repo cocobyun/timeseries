4e2c8edf45ed32be827237f4b004082eea456aed
cocobyun <suemin1192@gmail.com>
2022-08-02 10:01:14 +0900

    soyang flow prediction

--- samsungarimaR.R
+++ samsungarimaR.R
@@ -1,13 +1,25 @@
 data5 <-read.csv("D:/samsungm.csv")
 data5.ts = ts(data=data5$q,start=c(2010,01), frequency=12)
 data5.ts
+length(data5$num)
+findfrequency(data5)
+tsdisplay(data5$q)
 library("tseries")
+data5<-subset(data5,select = "q")
+
 summary(mo3<-arma(data5.ts, order=c(1,1)))
 kpss.test(data5.ts)
-fitted(mo3)
-plot(data5.ts,type="b",main="ARMA(1,1) Model")
-lines(fitted(mo3),col="red",lty=6,lwd=2)
-summary(am5<-auto.arima(data5.ts))
+
+summary(am4<-auto.arima(data5.ts))
+tsdiag(am4)
+
+
+forecast(am4)
+plot(forecast(am4),xlim=c(2018,2019))
+
+
+lines(fitted(am4),col="red",lty=2)
+summary(am3<-auto.arima(data$q2))
 
 x=matrix(0,5,7)
 x[6,6]
